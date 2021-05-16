22fcd5df346c0fffe503124e1b2a3e8b5b24d863
Chino Smitthikunanon <chinovieza@gmail.com>
2021-05-16 08:26:31 +0700

    Render by JSX

--- my-first-react/src/App.js
+++ my-first-react/src/App.js
@@ -4,11 +4,9 @@ import './App.css';
 
 function App() {
   return (
-    React.createElement(
-      'div',
-      {className: 'App'},
-      React.createElement('p', null, 'Hello World from App Component by React.createElement')
-    )
+    <div className='App'>
+      <p>Hello World by JSX</p>
+    </div>
   );
 }
 
