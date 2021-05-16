3ac3e66392608e4728dcdbfdba3051c67cf70c23
Chino Smitthikunanon <chinovieza@gmail.com>
2021-05-16 08:37:48 +0700

    Learn about JSX

--- my-first-react/src/App.js
+++ my-first-react/src/App.js
@@ -3,9 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  const myName = 'Monkey D. Luffy';
+  const data01 = 20;
+  const data02 = 15;
+  const randomNumber = Math.random()
+  const myJSX = (
+    <p>
+      Bounty: 1,500,000,000 Beli
+    </p>
+  )
+
   return (
     <div className='App'>
-      <p>Hello World by JSX</p>
+      <p>My name is : { myName }</p>
+      <p>Power is : { data01 + data02 }</p>
+      { randomNumber < 0.5 ? <div>Win</div> : <div>Lost</div> }
+      { myJSX }
     </div>
   );
 }
