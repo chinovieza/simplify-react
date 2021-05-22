757ac8f05130628652df051ef95e7363bf25f34b
Chino Smitthikunanon <chinovieza@gmail.com>
2021-05-22 11:02:25 +0700

    State on Function Component

--- my-first-react/src/App.js
+++ my-first-react/src/App.js
@@ -1,48 +1,25 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class App extends Component {
-    
-    state = {
-        user: "luffy",
-        password: "pirateking",
-        address: {
-            houseno: "123",
-            province: "Foosha Village"
-        }
-    };
+const App = (props) => {
 
-    userClick = () => {
-        this.setState({
-            user: "monky",
-            password: "GomuGomuNoMi"
-        });
-    }
+    const stateArr = useState({
+        home: '#home',
+        products: '#products',
+        profile: '#profile'
+    });
 
-    render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-12 col-sm-6 mx-auto mt-5">
-                        <div className="card ml-5 mr-5">
-                            <div className="card-header text-right">
-                                <button className="btn btn-success btn-sm" onClick={this.userClick}>
-                                    Click Here !
-                                </button>
-                            </div>
-                            <div className="card-body">
-                                user: <span className="ml-2">{this.state.user}</span><br/>
-                                password: <span className="ml-2">{this.state.password}</span><br/>
-                                house no: <span className="ml-2">{this.state.address.houseno}</span><br/>
-                                province: <span className="ml-1">{this.state.address.province}</span><br/>
-                            </div>
-                        </div>
-                    </div>
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav mx-auto">
+                    <a className="nav-item nav-link" href={stateArr[0].home}>Home</a>
+                    <a className="nav-item nav-link" href={stateArr[0].products}>Products</a>
+                    <a className="nav-item nav-link" href={stateArr[0].profile}>Profile</a>
                 </div>
             </div>
-        )
-    }
-
+        </nav>
+    );
 }
 
 export default App;
