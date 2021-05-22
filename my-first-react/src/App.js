66f2a28920349095098718dcfe5df5528944383a
Chino Smitthikunanon <chinovieza@gmail.com>
2021-05-22 11:05:12 +0700

    State on Function Component #2

--- my-first-react/src/App.js
+++ my-first-react/src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = (props) => {
 
-    const stateArr = useState({
+    const [dataState, setDataState] = useState({
         home: '#home',
         products: '#products',
         profile: '#profile'
@@ -13,9 +13,9 @@ const App = (props) => {
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav mx-auto">
-                    <a className="nav-item nav-link" href={stateArr[0].home}>Home</a>
-                    <a className="nav-item nav-link" href={stateArr[0].products}>Products</a>
-                    <a className="nav-item nav-link" href={stateArr[0].profile}>Profile</a>
+                    <a className="nav-item nav-link" href={dataState.home}>Home</a>
+                    <a className="nav-item nav-link" href={dataState.products}>Products</a>
+                    <a className="nav-item nav-link" href={dataState.profile}>Profile</a>
                 </div>
             </div>
         </nav>
