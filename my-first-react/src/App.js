a043c4e56f7f5371fd53f7b7fbba1b2735ae8628
Chino Smitthikunanon <chinovieza@gmail.com>
2021-05-22 09:50:27 +0700

    State on Class Component #2

--- my-first-react/src/App.js
+++ my-first-react/src/App.js
@@ -1,20 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
     
     state = {
-        id: '1',
-        name: 'Monkey D. Luffy'
+        user: "luffy",
+        password: "pirateking",
+        address: {
+            houseno: "123",
+            province: "Foosha Village"
+        }
+    };
+
+    userClick = () => {
+        this.setState({
+            user: "monky",
+            password: "GomuGomuNoMi"
+        });
     }
 
     render() {
         return (
-            <div>
-                { this.state.name }
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-sm-6 mx-auto mt-5">
+                        <div className="card ml-5 mr-5">
+                            <div className="card-header text-right">
+                                <button className="btn btn-success btn-sm" onClick={this.userClick}>
+                                    Click Here !
+                                </button>
+                            </div>
+                            <div className="card-body">
+                                user: <span className="ml-2">{this.state.user}</span><br/>
+                                password: <span className="ml-2">{this.state.password}</span><br/>
+                                house no: <span className="ml-2">{this.state.address.houseno}</span><br/>
+                                province: <span className="ml-1">{this.state.address.province}</span><br/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
-    
+
 }
 
 export default App;
