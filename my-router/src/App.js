4c2afa3ec70ce7a3809da7098faeb2b633595b26
Chino Smitthikunanon <chinovieza@gmail.com>
2021-06-06 08:40:38 +0700

    Use Link

--- my-router/src/App.js
+++ my-router/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Posts from './Posts';
 import Profile from './Profile';
 import Home from './Home';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const App = () => {
     return (
@@ -11,6 +11,11 @@ const App = () => {
                 <h2>Route App v.1</h2>
             </div>
             <div>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/posts">All Posts</Link></li>
+                    <li><Link to="/profile">User Profile</Link></li>
+                </ul>
                 <Route path="/posts" component={Posts} />
                 <Route path="/profile" component={Profile} />
                 <Route path="/home" component={Home} />
