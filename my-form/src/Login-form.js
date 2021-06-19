ea91789acf9e6c91a506b919dbde052281452f8c
Chino Smitthikunanon <chinovieza@gmail.com>
2021-06-19 08:50:55 +0700

    Prepare the components

---
+++ my-form/src/Login-form.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+
+    render() {
+        return (
+            <div>
+                Form in here !!!
+            </div>
+        )
+    }
+}
+
+export default LoginForm;
