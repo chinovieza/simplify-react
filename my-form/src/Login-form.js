388b467d8cb0bee2d6de140fa357a4e2b383dcb0
Chino Smitthikunanon <chinovieza@gmail.com>
2021-06-19 09:00:19 +0700

    Decoration login form

--- my-form/src/Login-form.js
+++ my-form/src/Login-form.js
@@ -4,8 +4,22 @@ class LoginForm extends Component {
 
     render() {
         return (
-            <div>
-                Form in here !!!
+            <div className="col-6 mt-5 mx-auto card">
+                <div className="card-body">
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" className="form-control" id="username" name="username" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" className="form-control" id="password" name="password" />
+                        </div>
+                        <div className="text-center">
+                            <button type="submit" className="btn btn-primary my-1">Login</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
