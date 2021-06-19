de201973108c60ed6083b2f730315b21a1adead5
Chino Smitthikunanon <chinovieza@gmail.com>
2021-06-19 09:15:00 +0700

    Set form state

--- my-form/src/Login-form.js
+++ my-form/src/Login-form.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 
 class LoginForm extends Component {
 
+    state = {
+        username: '',
+        password: ''
+    }
+
+    onInputChange = (event) => {
+
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+
+        console.log(this.state);
+
+    }
+
     render() {
         return (
             <div className="col-6 mt-5 mx-auto card">
@@ -9,11 +24,11 @@ class LoginForm extends Component {
                     <form>
                         <div className="form-group">
                             <label htmlFor="username">Username</label>
-                            <input type="text" className="form-control" id="username" name="username" />
+                            <input type="text" className="form-control" id="username" name="username" onChange={this.onInputChange} />
                         </div>
                         <div className="form-group">
                             <label htmlFor="password">Password</label>
-                            <input type="password" className="form-control" id="password" name="password" />
+                            <input type="password" className="form-control" id="password" name="password" onChange={this.onInputChange} />
                         </div>
                         <div className="text-center">
                             <button type="submit" className="btn btn-primary my-1">Login</button>
